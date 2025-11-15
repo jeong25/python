b6003a20bb6163433f12b8d0daa2df0c92ef64b8
jeong25 <yujinj0617@gmail.com>
2025-11-15 11:59:29 +0900

    Add files via upload

---
+++ 정유진.py
@@ -0,0 +1,44 @@
+'''
+def factorial(n):
+    output = 1
+    for i in range(1, n + 1):
+        output *= i
+    return output
+print("1! : ", factorial (1))
+print("2! : ", factorial (2))
+print("3! : ", factorial (3))
+print("4! : ", factorial (4))
+print("5! : ", factorial (5))
+'''
+'''
+def f(x):
+    return 2*x +1
+print(f(10))
+'''
+'''
+def mul(*values):
+    total = 1
+    for value in values:
+         total *= value
+    
+    return total
+print(mul(5, 7, 9, 10))
+'''
+'''
+def function(*values, valueA, valueB):
+    pass
+print(function(1, 2, 3, 4, 5))
+'''
+def function(*values, valueA=10, valueB = 20):
+    pass
+print(function(1, 2, 3, 4, 5))
+
+
+    
+
+'''
+def aaa(a=20,b,c):
+
+
+aaa(c=20, b=10
+'''
